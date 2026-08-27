5cef67c70feea05e09262f78c7f1d1be509fd1d9
Suyash Jain <suyashsjn@gmail.com>
2026-08-28 04:45:02 +0530

    Setup firebase & github actions deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Forces Next.js to generate static files
+  images: {
+    unoptimized: true, // Static export doesn't support the default Next.js Image Optimization API
+  },
 };
 
 export default nextConfig;
